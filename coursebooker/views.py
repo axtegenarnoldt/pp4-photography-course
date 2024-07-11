66b6524737601b93ccce931d77b3f894dd9468c1
Josefine Axtegen Arnoldt <axtegenarnoldt@gmail.com>
2024-07-11 17:47:53 +0000

    Add coursebooker urls.py, templates folder and base.html file

--- coursebooker/views.py
+++ coursebooker/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Course
 
 # Create your views here.
 
-def course_booker(request):
-    return HttpResponse("Hello, photograpers!")
+class PostList(generic.ListView):
+    queryset = Course.objects.all()
+    template_name = "base.html"
--- photocourse/urls.py
+++ photocourse/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from coursebooker.views import course_booker
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('coursebooker/', course_booker, name='coursebooker')
+    path("", include("coursebooker.urls"), name='coursebooker-urls')
 ]
