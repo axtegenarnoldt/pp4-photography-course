748a7f248eaa87068d5a729c38d34520f0805015
Josefine Axtegen Arnoldt <axtegenarnoldt@gmail.com>
2024-07-11 09:01:34 +0000

    change procfile

--- photocourse/settings.py
+++ photocourse/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h6jfufq_v6rgu)6zj6m0fvel*(@aub%l7v87(diaar!ju!0&7w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-axtegenarno-pp4photogra-qkftx7qwem7.ws.codeinstitute-ide.net', '.herokuapp.com']
 
