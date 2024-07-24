1a02a9e954a56e1d2571572c96db0d900dfcf51b
Josefine Axtegen Arnoldt <axtegenarnoldt@gmail.com>
2024-07-24 13:28:37 +0000

    add django summernote and migrations

--- coursebooker/admin.py
+++ coursebooker/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Course, Comment, Schedule, Bookings
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Course)
+class CourseAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status')
+    search_fields = ['title', 'content']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
 
 # Register your models here.
 
-admin.site.register(Course)
